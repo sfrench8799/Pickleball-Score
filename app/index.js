2aa9ec202e26b4bf6d020dc60625002655ea9620
Shane <sfrench8799@gmail.com>
2023-12-30 12:04:47 -0500

    Build server swap utility
    In progress, currently swaps "You" server side.

--- app/index.js
+++ app/index.js
@@ -6,26 +6,41 @@ const start = document.getElementById("start");
 const main = document.getElementById("main");
 const teamOneScore = document.getElementById("team-one-score");
 const teamTwoScore = document.getElementById("team-two-score");
+const teamOneServerOne = document.getElementById("team-one-server-one");
+const teamOneServerTwo = document.getElementById("team-one-server-two");
+const teamTwoServerOne = document.getElementById("team-two-server-one");
+const teamTwoServerTwo = document.getElementById("team-two-server-two");
 const servingScore = document.getElementById("serving-score");
 const nonServerScore = document.getElementById("non-serving-score");
 const serverLineOne = document.getElementById("server-line-1");
 const serverLineTwo = document.getElementById("server-line-2");
 const serverLineThree = document.getElementById("server-line-3");
 const serverLineFour = document.getElementById("server-line-4");
+const rallyWinnerButton = document.getElementById("winner-container");
+const rallyLoserButton = document.getElementById("loser-container");
 
 
 // Await input for who starts serving
 let startingTeam;
 let teamServing;
+let serverOne = "S1";
+let serverTwo = "S2";
 let yourTeamColor = "coral";
 let theirTeamColor = "lightskyblue";
+let rallyWinner;
+let currentServer;
 
-
+// Main game
 function startGame() {
     start.style.display = "none";
     main.style.display = "inline";
+    // Game setup
     setServerDashboard();
     setServingArrow();
+    currentServer = 1;
+    // Active game
+
+
 }
 
 // Adjust server dashboard based on who is serving
@@ -37,6 +52,7 @@ function setServerDashboard() {
     } else {
         servingScore.style.fill = theirTeamColor;
         nonServerScore.style.fill = yourTeamColor;
+        teamServing = 0;
     }
 }   
 
@@ -50,6 +66,37 @@ function setServingArrow() {
     }
 }
 
+function rallyComplete() {
+    if (rallyWinner && teamServing) {
+        serverSwap();
+    }
+}
+
+// Server change utility
+
+function serverSwap() {
+    if (teamServing) {
+        if (teamOneServerOne.text === "S1") {
+            teamOneServerOne.text = "S2";
+            teamOneServerTwo.text = "S1";
+        } else {
+            teamOneServerOne.text = "S1";
+            teamOneServerTwo.text = "S2";
+        }
+
+    } else {
+        if (teamTwoServerOne.text === "S1") {
+            teamTwoServerOne.text = "S2";
+            teamTwoServerTwo.text = "S1";
+        } else {
+            teamTwoServerOne.text = "S1";
+            teamTwoServerTwo.text = "S2";
+        }
+    }
+}
+
+// ************* Event Listeners *************
+
 // Start game select server
 
 you.addEventListener("mousedown", (evt) => {
@@ -62,5 +109,17 @@ them.addEventListener("mousedown", (evt) => {
     startingTeam = 0;
     teamServing = 0;
     startGame();
+})
+
+// Rally winner input
+
+rallyWinnerButton.addEventListener("mousedown", (evt) => {
+    rallyWinner = 1;
+    rallyComplete();
+})
+
+rallyLoserButton.addEventListener("mousedown", (evt) => {
+    rallyWinner = 0;
+    rallyComplete();
 })
 
